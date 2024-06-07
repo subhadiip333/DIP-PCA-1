297e4a81ba1a32038b93a8d4b2a15528cd7d023f
Subhadip Mahata <112509802+subhadiip333@users.noreply.github.com>
2024-06-07 21:36:30 +0530

    Add files via upload

---
+++ question2_c.m
@@ -0,0 +1,9 @@
+
+img = imread('moon.tif');
+
+
+negative_img = imcomplement(img);
+
+figure,
+subplot(1,2,1), imshow(img), title('Original Image')
+subplot(1,2,2), imshow(negative_img), title('Negative Image')
---
+++ question4.m
@@ -0,0 +1,7 @@
+img = imread('moon.tif');
+threshold = 100; 
+binary_img = img > threshold;
+
+figure,
+subplot(1,2,1), imshow(img), title('Original Image')
+subplot(1,2,2), imshow(binary_img), title(['Binary Image with Threshold ', num2str(threshold)])
